401ebbc295dfe3d8333a06d508e784f7407fcd23
Aidsman1234 <140405161+Aidsman1234@users.noreply.github.com>
2023-07-24 14:28:50 +0200

    Keigo added
    Polite form is better

--- cutie.py
+++ cutie.py
@@ -1,8 +1,9 @@
 import time
 
-lst=["cutie I want to eat your nose zehiiii"] 
+lst=["cutie I want to eat your nose zehiiii,kudasai"] 
 for word in lst:
     for letter in word[0:]:
         print(letter)
         time.sleep(0.2)
+print("fin!")
         
